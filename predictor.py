1f859334251a1ffd286ebaf9e53a439203fa06d8
Donatas <admin@l2topai.eu>
2017-01-29 22:12:35 +0200

    Missing import

--- predictor.py
+++ predictor.py
@@ -2,7 +2,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
-
+from keras.preprocessing import image
 
 # dimensions of our images.
 img_width, img_height = 256, 256
