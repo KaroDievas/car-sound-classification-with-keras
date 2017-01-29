b63cef437b30ab5671a857fd86906e3f245ddff9
Donatas <admin@l2topai.eu>
2017-01-29 16:46:58 +0200

    Added more layers and added correct paths to data

--- image_classificator_more_layers.py
+++ image_classificator_more_layers.py
@@ -7,11 +7,11 @@ from keras.layers import Activation, Dropout, Flatten, Dense
 # dimensions of our images.
 img_width, img_height = 256, 256
 
-train_data_dir = 'data/one_class/train'
-validation_data_dir = 'data/one_class/validation'
-nb_train_samples = 100
-nb_validation_samples = 100
-nb_epoch = 50
+train_data_dir = 'data/four_classes/train'
+validation_data_dir = 'data/four_classes/validation'
+nb_train_samples = 50
+nb_validation_samples = 50
+nb_epoch = 40
 
 
 model = Sequential()
@@ -40,10 +40,10 @@ model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
-model.add(Dense(64))
+model.add(Dense(256))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(1))
+model.add(Dense(4))
 model.add(Activation('sigmoid'))
 
 model.compile(loss='binary_crossentropy',
@@ -80,4 +80,4 @@ model.fit_generator(
         validation_data=validation_generator,
         nb_val_samples=nb_validation_samples)
 
-model.save_weights('first_try.h5')
+model.save_weights('third_try.h5')
