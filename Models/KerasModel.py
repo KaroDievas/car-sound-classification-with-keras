47175b028ada5c8c41c7105fbcc78624e1e60796
Donatas <admin@l2topai.eu>
2018-02-11 22:00:25 +0200

    adding git ignore and best model saving

---
+++ Models/KerasModel.py
@@ -0,0 +1,41 @@
+from keras.models import Sequential
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+
+
+class KerasModel:
+    def get_model(self):
+        img_width, img_height = 256, 256
+
+        model = Sequential()
+        model.add(Convolution2D(32, 3, 3, input_shape=(3, img_width, img_height)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Convolution2D(32, 3, 3))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Convolution2D(64, 3, 3))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Convolution2D(128, 3, 3))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Convolution2D(256, 3, 3))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Convolution2D(512, 3, 3))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Flatten())
+        model.add(Dense(256))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(4))
+        model.add(Activation('sigmoid'))
+        return model
---
+++ TrainModel.py
@@ -0,0 +1,59 @@
+from keras.preprocessing.image import ImageDataGenerator
+from Models.KerasModel import KerasModel
+from keras.callbacks import ModelCheckpoint
+import tensorflow as tf
+from keras import backend as K
+
+K.set_image_dim_ordering('th')
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+
+img_width, img_height = 256, 256
+train_data_dir = 'Data/big_pictures/train'
+validation_data_dir = 'Data/big_pictures/validation'
+nb_train_samples = 50
+nb_validation_samples = 50
+nb_epoch = 50
+
+kerasModel = KerasModel()
+model = kerasModel.get_model()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=5,
+    class_mode='categorical')
+
+class_dictionary = train_generator.class_indices
+
+print('class dictionary', class_dictionary)
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=5,
+    class_mode='categorical')
+
+check_pointer = ModelCheckpoint(filepath='weights.hdf5', verbose=1, save_best_only=True)
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples,
+    epochs=nb_epoch,
+    validation_data=validation_generator,
+    callbacks=[check_pointer])
