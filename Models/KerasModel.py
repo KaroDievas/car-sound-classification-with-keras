ce9b146a7476742e05f0fccac09d9d91a1b64c0d
KaroDievas <admin@l2topai.eu>
2018-03-14 20:53:05 +0200

    Keras model update for keras 2

--- Models/KerasModel.py
+++ Models/KerasModel.py
@@ -1,34 +1,39 @@
 from keras.models import Sequential
-from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 
 
 class KerasModel:
     def get_model(self, img_width, img_height):
         model = Sequential()
-        model.add(Convolution2D(32, 3, 3, input_shape=(3, img_width, img_height)))
+        model.add(Conv2D(32, (3, 3), input_shape=(3, img_width, img_height)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
 
-        model.add(Convolution2D(32, 3, 3))
+        model.add(Conv2D(32, (3, 3)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.3))
 
-        model.add(Convolution2D(64, 3, 3))
+        model.add(Conv2D(64, (3, 3)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.3))
 
-        model.add(Convolution2D(128, 3, 3))
+        model.add(Conv2D(128, (3, 3)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.3))
 
-        model.add(Convolution2D(256, 3, 3))
+        model.add(Conv2D(256, (3, 3)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.3))
 
-        model.add(Convolution2D(512, 3, 3))
+        model.add(Conv2D(512, (3, 3)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.3))
 
         model.add(Flatten())
         model.add(Dense(256))
